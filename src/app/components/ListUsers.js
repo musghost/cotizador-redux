2f3c5772c6341b44de7ddf534406fcc4c7dc7b50
Arturo Andrés Cidel Ortega <andres@cidel.com.mx>
2016-12-10 19:01:15 -0600

    Add delete-user-modal to list of users

--- src/app/components/ListUsers.js
+++ src/app/components/ListUsers.js
@@ -4,12 +4,27 @@ import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
 import moment from 'moment';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class ListUsers extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+      currentUser: null
+    }
+  }
 
   componentDidMount() {
     const {actions} = this.props;
     actions.getUsers();
+  }
+
+  selectUser = (x) => {
+
   }
 
   tableBody = () => {
@@ -27,26 +42,78 @@ class ListUsers extends Component {
     );
   }
 
+  handleClose = () => {
+    this.setState({
+      showModal: !this.state.showModal
+    });
+  }
+
+  dialog() {
+    const user = this.state.currentUser;
+
+    if(!user){
+      return null;
+    }
+
+    const actions = [
+      <FlatButton
+        label="Cancelar"
+        primary={true}
+        onTouchTap={this.handleClose}
+      />,
+      <FlatButton
+        label="Eliminar"
+        primary={true}
+        keyboardFocused={true}
+        onTouchTap={this.handleClose}
+      />,
+    ];
+
+    const modal = (
+      <Dialog
+        title="Eliminar al usuario {user.name}"
+        actions={actions}
+        modal={false}
+        open={true}
+        onRequestClose={this.handleClose}
+        >
+        <div>Realmente deseo eliminar al usuario:</div>
+        <div>
+          <div>{user.name}</div>
+          <div>{user.correo}</div>
+        </div>
+      </Dialog>
+    );
+
+    return showModal ? modal : null;
+  }
+
   renderTable() {
+
     return (
-      <Table selectable={Boolean(true)}>
-        <TableHeader displaySelectAll={Boolean(false)} adjustForCheckbox={Boolean(false)}>
-          <TableRow>
-            <TableHeaderColumn>Nombre</TableHeaderColumn>
-            <TableHeaderColumn>Correo</TableHeaderColumn>
-            <TableHeaderColumn>Fecha de creación</TableHeaderColumn>
-          </TableRow>
-        </TableHeader>
-        {this.tableBody()}
-      </Table>
+      <div>
+        <Table onCellClick={this.selectUser} selectable={Boolean(true)}>
+          <TableHeader displaySelectAll={Boolean(false)} adjustForCheckbox={Boolean(false)}>
+            <TableRow>
+              <TableHeaderColumn>Nombre</TableHeaderColumn>
+              <TableHeaderColumn>Correo</TableHeaderColumn>
+              <TableHeaderColumn>Fecha de creación</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+          {this.tableBody()}
+        </Table>
+        {this.dialog()}
+      </div>
     );
   }
 
   render() {
     const {loading} = this.props.users;
-    return (<div>
-      {loading ? (<h1>Loading</h1>) : this.renderTable()}
-    </div>);
+    return (
+      <div>
+        {loading ? (<h1>Loading</h1>) : this.renderTable()}
+      </div>
+    );
   }
 }
 
