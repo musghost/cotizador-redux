ee876125732bcacbe6002e160e0408ec1af8ea91
Arturo Andrés Cidel Ortega <andres@cidel.com.mx>
2016-12-20 10:36:43 -0600

    Remove old todo application

--- src/app/actions/index.js
+++ src/app/actions/index.js
@@ -4,30 +4,6 @@ import {config} from '../constants/Config';
 import uuid from 'uuid/v4';
 import {browserHistory} from 'react-router';
 
-export function addTodo(text) {
-  return {type: types.ADD_TODO, text};
-}
-
-export function deleteTodo(id) {
-  return {type: types.DELETE_TODO, id};
-}
-
-export function editTodo(id, text) {
-  return {type: types.EDIT_TODO, id, text};
-}
-
-export function completeTodo(id) {
-  return {type: types.COMPLETE_TODO, id};
-}
-
-export function completeAll() {
-  return {type: types.COMPLETE_ALL};
-}
-
-export function clearCompleted() {
-  return {type: types.CLEAR_COMPLETED};
-}
-
 export function toggleRegister(change) {
   return {type: types.TOGGLE_REGISTER, change};
 }
--- src/app/constants/ActionTypes.js
+++ src/app/constants/ActionTypes.js
@@ -1,10 +1,3 @@
-export const ADD_TODO = 'ADD_TODO';
-export const DELETE_TODO = 'DELETE_TODO';
-export const EDIT_TODO = 'EDIT_TODO';
-export const COMPLETE_TODO = 'COMPLETE_TODO';
-export const COMPLETE_ALL = 'COMPLETE_ALL';
-export const CLEAR_COMPLETED = 'CLEAR_COMPLETED';
-
 export const TOGGLE_REGISTER = 'TOGGLE_REGISTER';
 export const ADD_REGISTER_ERRORS = 'ADD_REGISTER_ERRORS';
 export const TOGGLE_LOGIN = 'TOGGLE_LOGIN';
